66bf50126d5ec3e4c060fea9a540b114914c2411
root <root@ip-172-31-4-157.us-east-2.compute.internal>
2020-09-08 20:35:05 +0000

    Update test

--- test/test-pages.js
+++ test/test-pages.js
@@ -10,11 +10,6 @@ describe("Status and content", function () {
         done();
       });
     });
-    it("status", function (done) {
-      request("http://localhost:8888/", function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-  });
 });
+  });
+
